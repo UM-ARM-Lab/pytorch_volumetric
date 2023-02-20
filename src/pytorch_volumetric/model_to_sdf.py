ec35aa0e177f3b55731431bd60b71cc4bcb8912d
Sheng Zhong <zhsh@umich.edu>
2023-02-20 18:02:50 -0500

    Convert robot description to SDF and test with pybullet

---
+++ src/pytorch_volumetric/model_to_sdf.py
@@ -0,0 +1,68 @@
+import typing
+import torch
+import pytorch_kinematics as pk
+from pytorch_volumetric import sdf
+import logging
+
+logger = logging.getLogger(__file__)
+
+
+class RobotSDF(sdf.ObjectFrameSDF):
+    """Create an SDF for a robot model described by a pytorch_kinematics Chain.
+    The SDF is conditioned on a joint configuration which must be set."""
+
+    def __init__(self, chain: pk.Chain, default_joint_config=None, path_prefix='',
+                 link_sdf_cls: typing.Callable[[sdf.ObjectFactory, ...], sdf.ObjectFrameSDF] = sdf.MeshSDF,
+                 **kwargs):
+        """
+
+        :param chain: Robot description; each link should be a mesh type - non-mesh geometries are ignored
+        :param default_joint_config: values for each joint of the robot by default; None results in all zeros
+        :param path_prefix: path to search for referenced meshes inside the robot description (e.g. URDF) which may use
+        relative paths. This given path is prefixed onto those relative paths in order to find the meshes.
+        :param link_sdf_cls: Factory of each link's SDFs; **kwargs are forwarded to this factory
+        :param kwargs: Keyword arguments fed to link_sdf_cls
+        """
+        self.chain = chain
+        self.dtype = self.chain.dtype
+        self.device = self.chain.device
+        self.q = None
+        self.object_to_link_frames: typing.Optional[pk.Transform3d] = None
+        self.joint_names = self.chain.get_joint_parameter_names()
+        self.frame_names = self.chain.get_frame_names()
+        self.sdf: typing.Optional[sdf.ComposedSDF] = None
+        self.sdf_to_link_name = []
+
+        sdfs = []
+        # get the link meshes from the frames and create meshes
+        for frame_name in self.frame_names:
+            frame = self.chain.find_frame(frame_name)
+            # TODO create SDF for non-mesh primitives
+            # TODO consider the visual offset transform
+            for link_vis in frame.link.visuals:
+                if link_vis.geom_type == "mesh":
+                    logger.info(f"{frame.link.name} offset {link_vis.offset}")
+                    link_obj = sdf.StubObjectFactory(link_vis.geom_param, path_prefix=path_prefix)
+                    link_sdf = link_sdf_cls(link_obj, **kwargs)
+                    self.sdf_to_link_name.append(frame.link.name)
+                    sdfs.append(link_sdf)
+                else:
+                    logger.warning(f"Cannot handle non-mesh link visual type {link_vis}")
+
+        self.sdf = sdf.ComposedSDF(sdfs, self.object_to_link_frames)
+        self.set_joint_configuration(default_joint_config)
+
+    def set_joint_configuration(self, joint_config=None):
+        """Set the joint configuration of the robot"""
+        if joint_config is None:
+            joint_config = torch.zeros(len(self.joint_names), device=self.device, dtype=self.dtype)
+        tf = self.chain.forward_kinematics(joint_config, end_only=False)
+        tsfs = []
+        for link_name in self.sdf_to_link_name:
+            tsfs.append(tf[link_name].get_matrix())
+        self.object_to_link_frames = pk.Transform3d(matrix=torch.cat(tsfs).inverse())
+        if self.sdf is not None:
+            self.sdf.obj_frame_to_each_frame = self.object_to_link_frames
+
+    def __call__(self, points_in_object_frame):
+        return self.sdf(points_in_object_frame)
---
+++ tests/test_model_to_sdf.py
@@ -0,0 +1,98 @@
+import os
+import math
+import torch
+import matplotlib.colors
+from matplotlib import pyplot as plt
+import numpy as np
+
+import pytorch_kinematics as pk
+from pytorch_volumetric.model_to_sdf import RobotSDF
+from pytorch_volumetric import voxel
+
+import pybullet as p
+import pybullet_data
+
+import logging
+
+plt.switch_backend('Qt5Agg')
+
+logging.basicConfig(level=logging.INFO, force=True,
+                    format='[%(levelname)s %(asctime)s %(pathname)s:%(lineno)d] %(message)s',
+                    datefmt='%m-%d %H:%M:%S')
+
+
+def test_urdf_to_sdf():
+    urdf = "kuka_iiwa/model.urdf"
+    search_path = pybullet_data.getDataPath()
+    # full_urdf = os.path.join(cfg.TEST_DIR, "kuka_iiwa.urdf")
+    full_urdf = os.path.join(search_path, urdf)
+    chain = pk.build_serial_chain_from_urdf(open(full_urdf).read(), "lbr_iiwa_link_7")
+    s = RobotSDF(chain, path_prefix=os.path.join(search_path, "kuka_iiwa"))
+    th = torch.tensor([0.0, -math.pi / 4.0, 0.0, math.pi / 2.0, 0.0, math.pi / 4.0, 0.0])
+
+    s.set_joint_configuration(th)
+
+    y = 0
+    query_range = np.array([
+        [-1, 0.5],
+        [-y, y],
+        [-0.2, 0.8],
+    ])
+
+    # toggles - g:GUI w:wireframe j:joint axis a:AABB i:interrupt
+    p.connect(p.GUI)
+    p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+    p.setAdditionalSearchPath(search_path)
+    armId = p.loadURDF(urdf, [0, 0, 0], useFixedBase=True)
+    # p.resetBasePositionAndOrientation(armId, [0, 0, 0], [0, 0, 0, 1])
+    for i, q in enumerate(th):
+        p.resetJointState(armId, i, q.item())
+
+    vis = None
+    try:
+        from base_experiments.env.env import draw_AABB
+        from base_experiments.env.pybullet_env import DebugDrawer
+        vis = DebugDrawer(1., 1.5)
+        vis.toggle_3d(True)
+        vis.set_camera_position([-0.1, 0, 0], yaw=-30, pitch=-20)
+        draw_AABB(vis, query_range)
+    except:
+        pass
+
+    coords, pts = voxel.get_coordinates_and_points_in_grid(0.01, query_range, device=s.device)
+    sdf_val, sdf_grad = s(pts)
+
+    norm = matplotlib.colors.Normalize(vmin=sdf_val.min().cpu() - 0.2, vmax=sdf_val.max().cpu())
+    color_map = matplotlib.cm.ScalarMappable(norm=norm)
+    rgb = color_map.to_rgba(sdf_val.reshape(-1).cpu())
+    rgb = rgb[:, :-1]
+
+    surface = sdf_val.abs() < 0.005
+    if vis is not None:
+        vis.draw_points("surface", pts[surface])
+
+    plt.ion()
+    plt.show()
+
+    def fmt(x):
+        s = f"{x:.1f}"
+        if s.endswith("0"):
+            s = f"{x:.0f}"
+        if x == 0:
+            return "surface"
+        return rf"{s}" if plt.rcParams["text.usetex"] else f"{s}"
+
+    ax = plt.gca()
+    ax.set_xlabel('x')
+    ax.set_ylabel('z')
+    x = coords[0]
+    z = coords[2]
+    v = sdf_val.reshape(len(x), len(z)).transpose(0, 1)
+    cset1 = ax.contourf(x, z, v, norm=norm, cmap='Greys_r')
+    cset2 = ax.contour(x, z, v, colors='k', levels=[0], linestyles='dashed')
+    ax.clabel(cset2, cset2.levels, inline=True, fontsize=13, fmt=fmt)
+    plt.draw()
+
+
+if __name__ == "__main__":
+    test_urdf_to_sdf()
