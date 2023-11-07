23983247ae66e87d502a328ea6b649a25dc5bdd9
tpower <tpower@umich.edu>
2023-11-07 12:52:42 -0500

    Use analytically calculated manipulator jacobian and hessian for improved performance

--- src/pytorch_volumetric/robot_scene.py
+++ src/pytorch_volumetric/robot_scene.py
@@ -33,19 +33,25 @@ class RobotScene:
         self.points_per_link = points_per_link
         self.softmin_temp = softmin_temp
         self.desired_link_idx = []
+        self.link_to_actuated_joint_idx = []
         if collision_check_links is not None:
             self.desired_links = collision_check_links
         else:
             self.desired_links = self.robot_sdf.sdf_to_link_name
 
+        self.link_to_actuated_joint_idx = []
         self.num_links = len(self.robot_sdf.sdf_to_link_name)
 
         self.scene_transform = scene_transform.to(device=self.device)
         self.robot_query_points, self._query_point_mask = self._generate_robot_query_points()
 
         self.transform_points = vmap(self._transform_points)
-        self.grad_points = vmap(jacrev(self._transform_points))
-        self.hess_points = vmap(jacfwd(jacrev(self._transform_points)))
+        self.transform_to_world = vmap(self._transform_to_world)
+        self.transform_world_to_scene = vmap(self._transform_world_to_scene)
+
+        self.grad_smooth_points = 10
+        # self.grad_points = vmap(jacrev(self._transform_points))
+        # self.hess_points = vmap(jacfwd(jacrev(self._transform_points)))
 
     def _get_desired_tfs(self):
         tfs = self.robot_sdf.sdf.obj_frame_to_link_frame.get_matrix().reshape(self.num_links, -1, 4, 4)
@@ -63,6 +69,7 @@ class RobotScene:
                 query_points.append(points)
                 self.desired_link_idx.append(i)
 
+        self.desired_link_idx = torch.tensor(self.desired_link_idx, device=self.device, dtype=torch.long)
         query_points = torch.stack(query_points, dim=0)
         # mask out points that are in self-collision with default configuration
         tfs = self._get_desired_tfs().inverse()
@@ -86,16 +93,32 @@ class RobotScene:
         # scene_mesh = self.scene_sdf.obj_factory._mesh.transform(self.scene_transform.get_matrix()[0].cpu().numpy())
         o3d.visualization.draw_geometries(pv.get_transformed_meshes(self.robot_sdf) + [pcd] + scene_meshes)
 
+    def _transform_to_world(self, q: torch.Tensor, weight=None):
+        """
+            Transforms robot query points from link frame to the scene_frame
+        :param q: torch.Tensor B x dq joint angles
+        :return:
+        """
+        self.robot_sdf.set_joint_configuration(q)
+        tfs = self._get_desired_tfs().inverse()
+        batched_query_points = self.robot_query_points
+        pts = tfs.transform_points(batched_query_points.reshape(-1, self.points_per_link, 3))
+        return pts
+
+    def _transform_world_to_scene(self, pts: torch.Tensor):
+        return self.scene_transform.inverse().transform_points(pts)
+
     def _transform_points(self, q: torch.Tensor, weight=None):
         self.robot_sdf.set_joint_configuration(q)
         tfs = self._get_desired_tfs().inverse()
         batched_query_points = self.robot_query_points
         pts = tfs.transform_points(batched_query_points.reshape(-1, self.points_per_link, 3))
-        pts = self.scene_transform.inverse().transform_points(pts)
+        pts_scene = self.scene_transform.inverse().transform_points(pts)
+
         if weight is None:
-            return pts.reshape(-1, 3)
+            return pts_scene.reshape(-1, 3)
         else:
-            return torch.sum(weight.reshape(-1, 1) * pts.reshape(-1, 3), dim=0)
+            return torch.sum(weight.reshape(-1, 1) * pts_scene.reshape(-1, 3), dim=0)
 
     def _collision_check(self, q: torch.Tensor, sdf: sdf.ObjectFrameSDF,
                          compute_gradient=False, compute_hessian=False):
@@ -115,10 +138,12 @@ class RobotScene:
         if compute_hessian and not compute_gradient:
             raise ValueError('Cannot compute hessian without gradient')
 
-        if compute_gradient:
-            q.requires_grad_(True)
-            q.retain_grad()
-        pts = self.transform_points(q)
+        #if compute_gradient:
+        #    q.requires_grad_(True)
+        #    q.retain_grad()
+
+        pts_world = self.transform_to_world(q)
+        pts = self.transform_world_to_scene(pts_world)
 
         if compute_hessian:
             sdf_vals, sdf_grads, sdf_hess = sdf.get_hessian(pts)
@@ -127,35 +152,73 @@ class RobotScene:
             sdf_vals, sdf_grads = sdf(pts)
             sdf_hess = None
 
+        # get minimum links
         sdf_vals = sdf_vals.reshape(B, -1)
         sdf_grads = sdf_grads.reshape(B, -1, 3)
-        # use softmin to get gradient but use hard min for actual sdf value
-        h = torch.softmax(-self.softmin_temp * sdf_vals, dim=1)
 
-        sdf_val = torch.min(sdf_vals, dim=1).values
+        sdf_indices = torch.argsort(sdf_vals, dim=1, descending=False)
+
+        # get closest point as the constraint
+        sdf_val = sdf_vals[torch.arange(B), sdf_indices[:, 0]]
+
+        # sdf_val, sdf_idx = torch.min(sdf_vals, dim=1)
 
         rvals = {
             'sdf': sdf_val,
         }
         if compute_gradient:
             # pts_jacobian = torch.sum(h[:, :, None, None] * self.grad_points(q, h), dim=1)  # B x 3 x 14
-            pts_jacobian = self.grad_points(q, h)  # B x 3 x 14
-            sdf_weighted_grad = torch.sum(h[:, :, None] * sdf_grads, dim=1)  # B x 3
-            q_grad = (pts_jacobian.transpose(1, 2) @ sdf_weighted_grad.unsqueeze(-1)).squeeze(-1)
-            rvals['grad_sdf'] = q_grad  # B x 14
+            # q is B x 14
+            # take points, transform back to world frame, then compute jacobian at these locations
+            # get the link indices for each point
+
+            # rather than take a single point we use the softmin to get a weighted average of the gradients for
+            # the self.grad_smooth_points number of closest points, this helps smooth the gradient
+            B_range = torch.arange(B).unsqueeze(-1)
+            closest_indices = sdf_indices[:, :self.grad_smooth_points]
+            closest_sdf_grads = sdf_grads[B_range, closest_indices]
+            closest_sdf_vals = sdf_vals[B_range, closest_indices]
+            h = torch.softmax(-self.softmin_temp * closest_sdf_vals, dim=1)
+
+            new_grad = True
+            pts_hessian = None
+            if new_grad:
+                closest_pts = pts.reshape(B, -1, 3)[B_range, closest_indices].reshape(-1, 3)
+                closest_links = self.desired_link_idx[closest_indices // self.points_per_link].reshape(-1)
+                q_repeat = q.unsqueeze(1).repeat(1, self.grad_smooth_points, 1).reshape(B*self.grad_smooth_points, -1)
+
+                if not compute_hessian:
+                    pts_jacobian = self.robot_sdf.chain.jacobian(q_repeat,
+                                                                 locations=closest_pts,
+                                                                 link_indices=closest_links)
+                else:
+                    pts_jacobian, pts_hessian = self.robot_sdf.chain.jacobian_and_hessian(q_repeat,
+                                                                                          locations=closest_pts,
+                                                                                          link_indices=closest_links)
+                    pts_hessian = pts_hessian[:, :3].reshape(B, self.grad_smooth_points, 3, q.shape[1], q.shape[1])
+
+                pts_jacobian = pts_jacobian[:, :3].reshape(B, self.grad_smooth_points, 3, -1)
+
+            else:
+                pts_jacobian = self.grad_points(q, h)  # B x 3 x 14
+
+            sdf_weighted_grad = h[:, :, None] * closest_sdf_grads
+            q_grad = (pts_jacobian.transpose(2, 3) @ sdf_weighted_grad.unsqueeze(-1)).squeeze(-1)
+            rvals['grad_sdf'] = torch.sum(q_grad, dim=1)  # B x 14
 
             if compute_hessian:
-                # hess_pts = torch.sum(h[:, :, None, None, None] * self.hess_points(q, h), dim=1)
-                hess_pts = self.hess_points(q, h)
-                sdf_weighted_hess = torch.sum(h[:, :, None, None] * sdf_hess, dim=1)
-                q_hess = torch.sum(hess_pts * sdf_weighted_grad.reshape(B, 3, 1, 1), dim=1)
-                q_hess2 = pts_jacobian.transpose(1, 2) @ sdf_weighted_hess @ pts_jacobian
+                if pts_hessian is None:
+                    pts_hessian = self.hess_points(q, h)
+                closest_sdf_hess = sdf_hess[B_range, closest_indices]
+                sdf_weighted_hess = h[:, :, None, None] * closest_sdf_hess
+                q_hess = torch.sum(pts_hessian * sdf_weighted_grad.reshape(B, -1, 3, 1, 1), dim=2)
+                q_hess2 = pts_jacobian.transpose(2, 3) @ sdf_weighted_hess @ pts_jacobian
                 q_hess = q_hess + q_hess2
-                rvals['hess_sdf'] = q_hess
+                rvals['hess_sdf'] = torch.sum(q_hess, dim=1)
 
-            self.robot_query_points.detach_()
-            self._query_point_mask.requires_grad_(False)
-            q = q.detach()
+            #self.robot_query_points.detach_()
+            #self._query_point_mask.requires_grad_(False)
+            #q = q.dstach()
 
         return rvals
 
