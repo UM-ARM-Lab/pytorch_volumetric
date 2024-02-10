Fix bug in gradient calculation, add sdf hessian for cylinder

src/pytorch_volumetric/robot_scene.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytorch_kinematics as pk
 from pytorch_volumetric import sdf, model_to_sdf
 import pytorch_volumetric as pv
+from pytorch_kinematics.transforms.rotation_conversions import euler_angles_to_matrix
 
 from torch.func import jacrev, jacfwd, hessian, vmap
 from functools import partial
@@ -49,7 +50,8 @@ class RobotScene:
         self.transform_to_world = vmap(self._transform_to_world)
         self.transform_world_to_scene = vmap(self._transform_world_to_scene)
 
-        self.grad_smooth_points = 10
+        self.transform_points_to_world = vmap(self._transform_pts_to_world)
+        self.grad_smooth_points = 50
         self.grad_points = vmap(jacrev(self._transform_points))
         # self.hess_points = vmap(jacfwd(jacrev(self._transform_points)))
 
@@ -119,6 +121,11 @@ class RobotScene:
         batched_query_points = self.robot_query_points
         pts = tfs.transform_points(batched_query_points.reshape(-1, self.points_per_link, 3))
         return pts
+
+    def _transform_pts_to_world(self, q, pts):
+        self.robot_sdf.set_joint_configuration(q)
+        tfs = self._get_desired_tfs().inverse()
+        return tfs.transform_points(pts.reshape(1, -1, 3))
 
     def _transform_world_to_scene(self, pts: torch.Tensor):
         return self.scene_transform.inverse().transform_points(pts)
@@ -288,7 +295,8 @@ class RobotScene:
             grad_h = -self.softmin_temp * (torch.diag_embed(h) * eye - h.unsqueeze(-1) * h.unsqueeze(-2))
 
             # want the closest points in the link frame
-            closest_pts = self.robot_query_points.repeat(B, 1, 1, 1).reshape(B, -1, 3)[B_range, closest_indices].reshape(-1, 3)
+            closest_pts = self.robot_query_points.repeat(B, 1, 1, 1).reshape(B, -1, 3)[
+                B_range, closest_indices].reshape(-1, 3)
             # closest points in world frame
             closest_pts_world = pts_world.reshape(B, -1, 3)[B_range, closest_indices]
 
@@ -298,14 +306,14 @@ class RobotScene:
                                                                                             -1)
             closest_env_links = sdf_frame_indices[B_range, closest_indices].reshape(-1)
 
-            #if not compute_hessian:
+            # if not compute_hessian:
             #    rob_jacobian = self.robot_sdf.chain.jacobian(q_repeat,
             #                                                 locations=closest_pts,
             #                                                 link_indices=closest_links)
             #    env_jacobian = self.scene_sdf.chain.jacobian(env_q_repeat,
             #                                                 locations=closest_pts,
             #                                                 link_indices=closest_env_links)
-            #else:
+            # else:
             # now need hessian always
             rob_jacobian, rob_hessian = self.robot_sdf.chain.jacobian_and_hessian(q_repeat,
                                                                                   locations=closest_pts,
@@ -314,6 +322,8 @@ class RobotScene:
             env_jacobian, env_hessian = self.scene_sdf.chain.jacobian_and_hessian(env_q_repeat,
                                                                                   locations=closest_pts,
                                                                                   link_indices=closest_env_links)
+            # rotation jacobian for environment
+            env_rot_jacobian = env_jacobian[:, 3:].reshape(B, self.grad_smooth_points, 3, -1)
 
             rob_hessian = rob_hessian[:, :3].reshape(B, self.grad_smooth_points, 3, q.shape[1], q.shape[1])
             env_hessian = env_hessian[:, :3].reshape(B, self.grad_smooth_points, 3, env_q.shape[1], env_q.shape[1])
@@ -324,12 +334,11 @@ class RobotScene:
             sdf_weighted_grad = h[:, :, None] * closest_sdf_grads
 
             # transform gradient to world frame
-            sdf_grad_world_frame = self.scene_transform.inverse().transform_normals(
+            sdf_grad_world_frame = self.scene_transform.transform_normals(
                 sdf_weighted_grad.reshape(-1, 3)).reshape(B, self.grad_smooth_points, 3)
 
-
             # sdf gradients in world frame
-            closest_sdf_grads_world = self.scene_transform.inverse().transform_normals(
+            closest_sdf_grads_world = self.scene_transform.transform_normals(
                 closest_sdf_grads.reshape(-1, 3)).reshape(B, self.grad_smooth_points, 3)
 
             # Compute gradient of closest point with respect to robot points in world frame
@@ -352,29 +361,108 @@ class RobotScene:
             rvals['closest_pt_world'] = torch.sum(h[:, :, None] * closest_pts_world, dim=1)  # B x 3
             rvals['closest_pt_q_grad'] = dclosest_dq  # B x 3 x 14
 
-            # alternative to doing this, we instead recompute contact jacobian and hessian at the closest point?
-            rvals['contact_jacobian'] = torch.sum(h[:, :, None, None] * rob_jacobian, dim=1) # B x 3 x 16
+            ## alternative to doing this, we instead recompute contact jacobian and hessian at the closest point?
+            rvals['contact_jacobian'] = torch.sum(h[:, :, None, None] * rob_jacobian, dim=1)  # B x 3 x 16
+            ## now want to get the contact hessian dJ/dq
+            rvals['contact_hessian'] = torch.sum(h[:, :, None, None, None] * rob_hessian, dim=1)  # B x 3 x 14 x 14
 
-            # now want to get the contact hessian dJ/dq
-            rvals['contact_hessian'] = torch.sum(h[:, :, None, None, None] * rob_hessian, dim=1) # B x 3 x 14 x 14
-
+            # get contact normals
             rvals['contact_normal'] = torch.sum(sdf_grad_world_frame, dim=1)
             rvals['contact_normal'] = rvals['contact_normal'] / torch.norm(rvals['contact_normal'], dim=1, keepdim=True)
 
-            if compute_hessian:
-                closest_sdf_hess = sdf_hess[B_range, closest_indices]
-                sdf_weighted_hess = h[:, :, None, None] * closest_sdf_hess
-                sdf_hess_world_frame = self.scene_transform.transform_shape_operator(
-                    sdf_weighted_hess.reshape(-1, 3, 3)).reshape(B, self.grad_smooth_points, 3, 3)
+            closest_sdf_hess = sdf_hess[B_range, closest_indices]
+            sdf_weighted_hess = h[:, :, None, None] * closest_sdf_hess
+            sdf_hess_world_frame = self.scene_transform.transform_shape_operator(
+                sdf_weighted_hess.reshape(-1, 3, 3)).reshape(B, self.grad_smooth_points, 3, 3)
 
-                q_hess = torch.sum(rob_hessian * sdf_grad_world_frame.reshape(B, -1, 3, 1, 1), dim=2)
-                q_hess2 = rob_jacobian.transpose(2, 3) @ sdf_hess_world_frame @ rob_jacobian
-                q_hess = q_hess + q_hess2
-                rvals['hess_sdf'] = torch.sum(q_hess, dim=1)
-                q_env_hess = torch.sum(env_hessian * -sdf_weighted_grad.reshape(B, -1, 3, 1, 1), dim=2)
-                q_env_hess2 = env_jacobian.transpose(2, 3) @ -sdf_weighted_hess @ env_jacobian
-                q_env_hess = q_env_hess + q_env_hess2
-                rvals['hess_env_sdf'] = torch.sum(q_env_hess, dim=1)
+            rvals['dnormal_dq'] = torch.sum(sdf_hess_world_frame @ rob_jacobian, dim=1)
+
+            # get contact normal in scene frame
+            contact_normal_scene = torch.sum(sdf_weighted_grad, dim=1)
+            contact_normal_scene = contact_normal_scene / torch.linalg.norm(contact_normal_scene, dim=-1, keepdim=True)
+
+            rvals['env_rot_jacobian'] = torch.sum(h[:, :, None, None] * env_rot_jacobian, dim=1)
+            dnormal_denv_q_rigid = torch.cross(contact_normal_scene.reshape(-1, 1, 3),
+                                               rvals['env_rot_jacobian'].transpose(2, 1), dim=-1).transpose(1, 2)
+
+            # this is the gradient of the contact normal in the scene frame, need it in robot frame
+            dnormal_denv_q = torch.sum(sdf_weighted_hess @ env_jacobian, dim=1) + dnormal_denv_q_rigid
+            rvals['dnormal_denv_q'] = self.scene_transform.transform_normals(
+                dnormal_denv_q.transpose(1, 2).reshape(-1, 3)
+            ).reshape(B, -1, 3).transpose(1, 2)
+
+            # # # let's check if SDF hessian is correct
+            # # # try a test
+            # eps = 1e-3
+            # hess2 = torch.zeros_like(sdf_hess_world_frame)
+            # print(hess2.shape)
+            # closest_pts_scene = pts.reshape(B, -1, 3)[B_range, closest_indices]
+            # print(closest_pts_scene.shape)
+            # for i in range(3):
+            #     d = torch.zeros_like(closest_pts_scene).reshape(B, -1, 3)
+            #     d[..., i] = eps
+            #     # print(d.shape, closest_pts.shape, (closest_pts + d).shape)
+            #
+            #     # plus = self.transform_points_to_world(q, closest_pts.reshape(B, -1, 3) + d).reshape(B, -1, 3)
+            #     # plus = self.transform_world_to_scene(plus)
+            #     # neg = self.transform_points_to_world(q, closest_pts.reshape(B, -1, 3) - d).reshape(B, -1, 3)
+            #
+            #     plus = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) + d)
+            #     neg = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) - d)
+            #
+            #     ret = sdf(plus, return_extra_info=True)
+            #     sdf_plus_grads = ret['sdf_grad']
+            #     ret = sdf(neg, return_extra_info=True)
+            #     sdf_minus_grads = ret['sdf_grad']
+            #     hess2[..., i] = (sdf_plus_grads - sdf_minus_grads) / (2 * eps)
+            #
+            # hess3 = torch.zeros_like(hess2)
+            # for i in range(3):
+            #     for j in range(3):
+            #         if i == j:
+            #             d = torch.zeros_like(closest_pts_scene).reshape(B, -1, 3)
+            #             d[..., i] = eps
+            #             neutral = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3))
+            #             plus = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) + d)
+            #             neg = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) - d)
+            #
+            #             ret = sdf(neutral, return_extra_info=True)
+            #             sdf_neutral = ret['sdf_val']
+            #             ret = sdf(plus, return_extra_info=True)
+            #             sdf_plus = ret['sdf_val']
+            #             ret = sdf(neg, return_extra_info=True)
+            #             sdf_minus = ret['sdf_val']
+            #
+            #             hess_val = (sdf_plus - 2 * sdf_neutral + sdf_minus) / (eps * eps)
+            #         else:
+            #             dsame = torch.zeros_like(closest_pts_scene).reshape(B, -1, 3)
+            #             ddiff = dsame.clone()
+            #
+            #             dsame[..., i] = eps
+            #             dsame[..., j] = eps
+            #             ddiff[..., i] = eps
+            #             ddiff[..., j] = -eps
+            #             pp = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) + dsame)
+            #             pm = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) + ddiff)
+            #             mp = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) - ddiff)
+            #             mm = self.transform_world_to_scene(closest_pts_world.reshape(B, -1, 3) - dsame)
+            #
+            #             pp = sdf(pp, True)['sdf_val']
+            #             pm = sdf(pm, True)['sdf_val']
+            #             mp = sdf(mp, True)['sdf_val']
+            #             mm = sdf(mm, True)['sdf_val']
+            #
+            #             hess_val = (pp - pm - mp + mm) / (4 * eps * eps)
+            #         hess3[..., i, j] = hess_val
+
+            q_hess = torch.sum(rob_hessian * sdf_grad_world_frame.reshape(B, -1, 3, 1, 1), dim=2)
+            q_hess2 = rob_jacobian.transpose(2, 3) @ sdf_hess_world_frame @ rob_jacobian
+            q_hess = q_hess + q_hess2
+            rvals['hess_sdf'] = torch.sum(q_hess, dim=1)
+            q_env_hess = torch.sum(env_hessian * -sdf_weighted_grad.reshape(B, -1, 3, 1, 1), dim=2)
+            q_env_hess2 = env_jacobian.transpose(2, 3) @ -sdf_weighted_hess @ env_jacobian
+            q_env_hess = q_env_hess + q_env_hess2
+            rvals['hess_env_sdf'] = torch.sum(q_env_hess, dim=1)
 
         return rvals
 

src/pytorch_volumetric/sdf.py
@@ -455,49 +455,112 @@ class CylinderSDF(ObjectFrameSDF):
         return torch.tensor([[-self.r - padding, -self.l - padding, -self.r - padding],
                              [self.r + padding, self.l + padding, self.r + padding]])
 
-    def _get_sdf(self, points_in_object_frame):
-        rounded_rad = 1e-6
+    def _project_to_cylinder(self, points_in_object_frame):
 
         p_xy = points_in_object_frame[..., :2]
-        p_y = points_in_object_frame[..., 2]
+        p_z = points_in_object_frame[..., 2]
 
-        diff = torch.zeros_like(p_xy)
-        diff[..., 0] = torch.linalg.norm(p_xy, dim=-1) - self.r + rounded_rad
-        diff[..., 1] = torch.abs(p_y) - self.l
+        # determine which octant we are in
+        # first compute which octant we are in
+        octant = torch.sign(points_in_object_frame)
+        # absolute value z for now
+        p_z_abs = torch.abs(p_z)
 
-        sdf_value = torch.clamp(torch.max(diff, dim=-1).values, max=0)
-        sdf_value = sdf_value + torch.linalg.norm(torch.clamp(diff, min=0), dim=-1) - rounded_rad
-        return sdf_value
+        # project to disc
+        new_z = torch.clamp(p_z_abs, min=None, max=self.l)
 
-    def _get_sdf_grad(self, points_in_object_frame):
+        d_to_xy = torch.linalg.norm(p_xy, dim=-1)
+        d_to_disc = p_z_abs - self.l  # for negative components
+
+        # points outside the cylinder projected to cylinder
+        new_xy = p_xy / d_to_xy.unsqueeze(-1) * self.r
+
+        # points inside the cylinder projected to cylinder
+        d_to_xy = d_to_xy.unsqueeze(-1)
+        d_to_disc = d_to_disc.unsqueeze(-1)
+        new_xy = torch.where(d_to_xy < self.r,
+                             torch.where(d_to_disc > 0, p_xy,
+                                         torch.where(self.r - d_to_xy > torch.abs(d_to_disc), p_xy, new_xy)), new_xy)
+
+        in_flag = torch.where(d_to_xy > self.r, 1, -1)
+        in_flag = torch.where(d_to_disc > 0, 1, in_flag)
+
+        d_to_xy = d_to_xy.squeeze(-1)
+        d_to_disc = d_to_disc.squeeze(-1)
+        new_z = torch.where(d_to_xy < self.r,
+                            torch.where(d_to_disc > 0, self.l,
+                                        torch.where(self.r - d_to_xy > torch.abs(d_to_disc), self.l, new_z)), new_z)
+
+        # get sign for z component
+        new_z = new_z * octant[..., 2]
+
+        new_points = torch.cat((new_xy, new_z.unsqueeze(-1)), dim=-1)
+        grad = (new_points - points_in_object_frame)
+        sdf_val = in_flag * torch.linalg.norm(grad, dim=-1, keepdim=True)
+
+        # if we are very close to the surface, should use surface normal instead
+        # need to first figure out if we are on the disc or if we are on the side
+        z_vector = torch.tensor([0., 0., 1.], device=self.device).expand_as(points_in_object_frame)
+        xy_vector = torch.cat((p_xy / d_to_xy.unsqueeze(-1), torch.zeros(*p_xy.shape[:-1], 1,
+                                                                         device=self.device)), dim=-1)
+        z_vector = z_vector * octant[..., 2, None]
+        grad = torch.where(sdf_val.abs() < 1e-6,
+                           torch.where((d_to_disc.unsqueeze(-1)).abs() < 1e-6, z_vector, xy_vector), -grad / sdf_val)
+        return sdf_val.squeeze(-1), grad
+
+    def _get_sdf_hess(self, points_in_object_frame):
         # TODO: it is probably possible to do this analytically, use finite differencing for now
-        h = 1e-4
-        N = len(points_in_object_frame) - 1
+        # alternative grad method
+
+        h = 1e-6
+        N = len(points_in_object_frame.shape) - 1
         perturbation = h * torch.stack([torch.eye(3, device=self.device),
                                         -torch.eye(3, device=self.device)], dim=1).reshape(-1, 3)
 
         expand = [1] * N
         # perturb the points
         pts_perturbed = points_in_object_frame[..., None, :] + perturbation.reshape(*expand, -1, 3)
-        pts_perturbed = pts_perturbed.reshape(-1, 3, 2, 3).permute(0, 2, 3, 1)
+        pts_perturbed = pts_perturbed.reshape(-1, 3, 2, 3).permute(0, 2, 1, 3)
+        _, sdf_grads = self._project_to_cylinder(pts_perturbed.reshape(-1, 3))  # .reshape(-1, 2, 3)
+        sdf_grads = sdf_grads.reshape(-1, 2, 3, 3)
+        hess = (sdf_grads[:, 0] - sdf_grads[:, 1]) / (2 * h)
 
-        sdf_vals = self._get_sdf(pts_perturbed.reshape(-1, 3)).reshape(-1, 2, 3)
-        grad = (sdf_vals[:, 0, :] - sdf_vals[:, 1, :]) / (2 * h)
-        norm_grad = torch.linalg.norm(grad, dim=-1, keepdim=True)
-        # normalize
-        grad = torch.where(norm_grad > 1e-4, grad / norm_grad, torch.zeros_like(grad))
-        return grad
+        # hess should be symmetric
+        hess = (hess + hess.permute(0, 2, 1)) / 2
+
+        # # try a test
+        # hess2 = torch.zeros_like(hess)
+        # for i in range(3):
+        #     d = torch.zeros_like(points_in_object_frame)
+        #     d[..., i] = h
+        #     print(d[0])
+        #
+        #     _, sdf_grads_plus = self._project_to_cylinder(points_in_object_frame + d)
+        #     _, sdf_grads_minus = self._project_to_cylinder(points_in_object_frame - d)
+        #
+        #     print(sdf_grads_minus.shape, sdf_grads_plus.shape, hess2[..., i].shape)
+        #     hess2[..., i] = (sdf_grads_plus - sdf_grads_minus).reshape(-1, 3) / (2 * h)
+        #
+        # print(hess)
+        # print(hess2)
+        # print(torch.max(torch.abs(hess - hess2)))
+        # print(torch.max(hess.abs()))
+        # print(torch.min(hess2.abs()))
+        # exit(0)
+        return hess
 
     def __call__(self, points_in_object_frame, return_extra_info=False):
         N, d = points_in_object_frame.shape[-2:]
 
-        sdf_value = self._get_sdf(points_in_object_frame)
-        sdf_grad = self._get_sdf_grad(points_in_object_frame)
+        # sdf_value = self._get_sdf(points_in_object_frame)
+        # sdf_grad = self._get_sdf_grad(points_in_object_frame)
+        sdf_value, sdf_grad = self._project_to_cylinder(points_in_object_frame)
 
+        sdf_hess = self._get_sdf_hess(points_in_object_frame)
         # TODO: Including radius of corners results in a non-zero hessian around corners, perhaps compute that?
         sdf_value = sdf_value.reshape(*points_in_object_frame.shape[:-1])
         sdf_grad = sdf_grad.reshape(*points_in_object_frame.shape[:-1], d)
-        sdf_hess = torch.zeros(*points_in_object_frame.shape[:-1], d, d, device=points_in_object_frame.device)
+        sdf_hess = sdf_hess.reshape(*points_in_object_frame.shape[:-1], d, d)
 
         # points are transformed to link frame, thus it needs to compare against the object in link frame
         # objId is not in link frame and shouldn't be moved
@@ -711,13 +774,11 @@ class ComposedSDF(ObjectFrameSDF):
             v = sdf_result['sdf_val']
             g = sdf_result['sdf_grad']
             h = sdf_result['sdf_hess']
-
             # need to transform the gradient back to the object frame
             g = self.link_frame_to_obj_frame[i].transform_normals(g)
 
             # transform the hessian also
             h = self.link_frame_to_obj_frame[i].transform_shape_operator(h)
-
             sdfv.append(v)
             sdfg.append(g)
             sdfh.append(h)