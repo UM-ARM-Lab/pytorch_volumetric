365e3feff9942d46828ac3e7f86d0d2b97dddf14
Fan Yang <fanyangr@umich.edu>
2024-06-10 13:32:56 -0400

    fix a tiny bug, the link for collision checking should be queried in the given order

--- src/pytorch_volumetric/robot_scene.py
+++ src/pytorch_volumetric/robot_scene.py
@@ -63,41 +63,42 @@ class RobotScene:
 
     def _generate_robot_query_points(self, partial_patch=False):
         query_points = []
-        for i, link_name in enumerate(self.robot_sdf.sdf_to_link_name):
-            if link_name in self.desired_links:
-                link_sdf = self.robot_sdf.get_link_sdf(link_name)
-                # link_sdf.precompute_sdf()
-                # points, _, _ = sdf.sample_mesh_points(link_sdf, self.points_per_link,
-                #                                      dbpath=f'{link_name}_points_cache.pkl', device=self.device)
-                points, _ = link_sdf.sample_surface_points(self.points_per_link,
-                                                           dbpath=f'{link_name}_points_cache.pkl', device=self.device)
+        for target_link_name in self.desired_links:
+            for i, link_name in enumerate(self.robot_sdf.sdf_to_link_name):
+                if link_name == target_link_name:
+                    link_sdf = self.robot_sdf.get_link_sdf(link_name)
+                    # link_sdf.precompute_sdf()
+                    # points, _, _ = sdf.sample_mesh_points(link_sdf, self.points_per_link,
+                    #                                      dbpath=f'{link_name}_points_cache.pkl', device=self.device)
+                    points, _ = link_sdf.sample_surface_points(self.points_per_link,
+                                                            dbpath=f'{link_name}_points_cache.pkl', device=self.device)
 
-                if partial_patch:
-                    ee_names = ['allegro_hand_hitosashi_finger_finger_0_aftc_base_link',
-                    'allegro_hand_naka_finger_finger_1_aftc_base_link',
-                    'allegro_hand_kusuri_finger_finger_2_aftc_base_link',
-                    'allegro_hand_oya_finger_3_aftc_base_link']
-                    loc1, = torch.where(points[:, 1] > 0.005)
-                    points = points[loc1]
-                    loc2, = torch.where(points[:, 2] > 0.01)
-                    points = points[loc2]
-                    while points.shape[0] < self.points_per_link:
-                        new_points, _ = link_sdf.sample_surface_points(self.points_per_link,
-                                                                       dbpath=f'{link_name}_points_cache.pkl', device=self.device)
-                        loc1, = torch.where(new_points[:, 1] > 0.005)
-                        new_points = new_points[loc1]
-                        if link_name in ee_names:
-                            loc2, = torch.where(new_points[:, 2] > 0.018)
-                        else:
-                            loc2, = torch.where(new_points[:, 2] > 0.01)
-                        new_points = new_points[loc2]
-                        points = torch.cat([points, new_points], dim=0)     
-                    points = points[:self.points_per_link]
-
-                query_points.append(points)
-                # TODO: confusing because index from robot_sdf and from chain are not the same, perhaps unify them?
-                self.desired_link_idx.append(i)
-                self.desired_frame_idx.append(self.robot_sdf.chain.frame_to_idx[link_name])
+                    if partial_patch:
+                        ee_names = ['allegro_hand_hitosashi_finger_finger_0_aftc_base_link',
+                        'allegro_hand_naka_finger_finger_1_aftc_base_link',
+                        'allegro_hand_kusuri_finger_finger_2_aftc_base_link',
+                        'allegro_hand_oya_finger_3_aftc_base_link']
+                        if link_name in ee_names: # only do that for finger tips
+                            loc1, = torch.where(points[:, 1] > 0.005)
+                            points = points[loc1]
+                            loc2, = torch.where(points[:, 2] > 0.01)
+                            points = points[loc2]
+                            while points.shape[0] < self.points_per_link:
+                                new_points, _ = link_sdf.sample_surface_points(self.points_per_link,
+                                                                            dbpath=f'{link_name}_points_cache.pkl', device=self.device)
+                                loc1, = torch.where(new_points[:, 1] > 0.005)
+                                new_points = new_points[loc1]
+                                # if link_name in ee_names:
+                                #     loc2, = torch.where(new_points[:, 2] > 0.018)
+                                # else:
+                                loc2, = torch.where(new_points[:, 2] > 0.01)
+                                new_points = new_points[loc2]
+                                points = torch.cat([points, new_points], dim=0)     
+                            points = points[:self.points_per_link]
+                    query_points.append(points)
+                    # TODO: confusing because index from robot_sdf and from chain are not the same, perhaps unify them?
+                    self.desired_link_idx.append(i)
+                    self.desired_frame_idx.append(self.robot_sdf.chain.frame_to_idx[link_name])
         self.desired_link_idx = torch.tensor(self.desired_link_idx, device=self.device, dtype=torch.long)
         self.desired_frame_idx = torch.tensor(self.desired_frame_idx, device=self.device, dtype=torch.long)
 
