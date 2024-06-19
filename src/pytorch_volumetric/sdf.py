4c45f38fbdc51685cbef6f4a529e448fe1c59772
Fan Yang <fanyangr@umich.edu>
2024-06-19 13:04:25 -0400

    add URDFSDF

--- src/pytorch_volumetric/sdf.py
+++ src/pytorch_volumetric/sdf.py
@@ -18,6 +18,7 @@ from pytorch_volumetric.voxel import VoxelGrid, get_divisible_range_by_resolutio
 import pytorch_kinematics as pk
 import logging
 
+
 logger = logging.getLogger(__name__)
 
 
@@ -1004,6 +1005,127 @@ class CachedSDF(ObjectFrameSDF):
 
     def sample_surface_points(self, num_points, **kwargs):
         return self.gt_sdf.sample_surface_points(num_points, **kwargs)
+
+class URDFSDF(ObjectFrameSDF):
+    """Create an SDF for a robot model described by a pytorch_kinematics Chain.
+    The SDF is conditioned on a joint configuration which must be set."""
+
+    def __init__(self, chain: pk.Chain, path_prefix='',
+                 link_sdf_cls: typing.Callable[[ObjectFactory], ObjectFrameSDF] = MeshSDF,
+                 use_collision_geometry=False, **kwargs):
+        """
+
+        :param chain: Robot description; each link should be a mesh type - non-mesh geometries are ignored
+        :param default_joint_config: values for each joint of the robot by default; None results in all zeros
+        :param path_prefix: path to search for referenced meshes inside the robot description (e.g. URDF) which may use
+        relative paths. This given path is prefixed onto those relative paths in order to find the meshes.
+        :param link_sdf_cls: Factory of each link's SDFs; **kwargs are forwarded to this factory
+        :param use_collision_geometry: If True, use the collision geometry instead of the visual geometry
+        :param kwargs: Keyword arguments fed to link_sdf_cls
+
+        """
+        self.chain = chain
+        self.dtype = self.chain.dtype
+        self.device = self.chain.device
+        self.q = None
+        self.object_to_link_frames: typing.Optional[pk.Transform3d] = None
+        self.joint_names = self.chain.get_joint_parameter_names()
+        self.frame_names = self.chain.get_frame_names(exclude_fixed=False)
+        self.sdf_index_to_frame_index = []
+        self.sdf: typing.Optional[ComposedSDF] = None
+        self.sdf_to_link_name = []
+        self.configuration_batch = None
+
+        sdfs = []
+        offsets = []
+        # get the link meshes from the frames and create meshes
+        for frame_name in self.frame_names:
+            frame = self.chain.find_frame(frame_name)
+            # TODO create SDF for non-mesh primitives
+            # TODO consider the visual offset transform
+            supported_types = ["mesh", "box", "sphere", "cylinder", None]
+
+            if use_collision_geometry:
+                link_geometries = frame.link.collisions
+            else:
+                link_geometries = frame.link.visuals
+
+            for link_geom in link_geometries:
+                if link_geom.geom_type not in supported_types:
+                    logger.warning(f"Cannot handle unsupported link visual type {link_geom}")
+                else:
+                    if link_geom.geom_type == "mesh":
+                        logger.info(f"{frame.link.name} offset {link_geom.offset}")
+                        link_obj = MeshObjectFactory(link_geom.geom_param[0],
+                                                         scale=link_geom.geom_param[1],
+                                                         path_prefix=path_prefix)
+                        link_sdf = link_sdf_cls(link_obj)
+                    elif link_geom.geom_type == "box":
+                        link_sdf = BoxSDF(link_geom.geom_param, device=self.device)
+                    elif link_geom.geom_type == "cylinder":
+                        link_sdf = CylinderSDF(link_geom.geom_param[0],
+                                                   link_geom.geom_param[1], device=self.device)
+                    elif link_geom.geom_type == "sphere":
+                        link_sdf = SphereSDF(link_geom.geom_param, device=self.device)
+                    else:
+                        # this is the None type
+                        continue
+                    self.sdf_to_link_name.append(frame.link.name)
+                    self.sdf_index_to_frame_index.append(self.chain.frame_to_idx[frame_name])
+                    sdfs.append(link_sdf)
+                    offsets.append(link_geom.offset)
+
+        self.offset_transforms = offsets[0].stack(*offsets[1:]).to(device=self.device, dtype=self.dtype)
+        self.sdf = ComposedSDF(sdfs, self.offset_transforms.inverse())
+        self.sdf_index_to_frame_index = torch.tensor(self.sdf_index_to_frame_index, device=self.device)
+
+    def surface_bounding_box(self, padding=0.):
+        return self.sdf.surface_bounding_box(padding=padding)
+
+    def link_bounding_boxes(self):
+        """
+        Get the bounding box of each link in the robot's frame under the current configuration.
+        Note that the bounding box is not necessarily axis-aligned, so the returned bounding box is not just
+        the min and max of the points.
+        :return: [A x] [B x] 8 x 3 points of the bounding box for each link in the robot's frame
+        """
+        tfs = self.sdf.obj_frame_to_link_frame.inverse()
+        bbs = []
+        for i in range(len(self.sdf.sdfs)):
+            sdf = self.sdf.sdfs[i]
+            bb = aabb_to_ordered_end_points(sdf.surface_bounding_box(padding=0))
+            bb = tfs.transform_points(torch.tensor(bb, device=tfs.device, dtype=tfs.dtype))[
+                self.sdf.ith_transform_slice(i)]
+            bbs.append(bb)
+        return torch.stack(bbs).squeeze()
+
+    def get_link_sdf(self, link_name) -> ObjectFrameSDF:
+        """
+        Get the object factory for the given link
+        :param link_name: name of the link
+        :return: object factory for the link
+        """
+        return self.sdf.sdfs[self.sdf_to_link_name.index(link_name)]
+
+    def __call__(self, points_in_object_frame, return_extra_info=False):
+        """
+        Query for SDF value and SDF gradients for points in the robot's frame
+        :param points_in_object_frame: [B x] N x 3 optionally arbitrarily batched points in the robot frame; B can be
+        any number of batch dimensions.
+        :return: [A x] [B x] N SDF value, and [A x] [B x] N x 3 SDF gradient. A are the configurations' arbitrary
+        number of batch dimensions.
+        """
+        return self.sdf(points_in_object_frame, return_extra_info)
+
+    def precompute_sdf(self):
+        self.sdf.precompute_sdf()
+
+    def sample_surface_points(self, num_points, **kwargs):
+        raise NotImplementedError()
+
+    def get_mesh_list(self):
+        return self.sdf.get_mesh_list()
+
 
 
 def sample_mesh_points(obj_factory: ObjectFactory = None, num_points=100, seed=0, name="",
